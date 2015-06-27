5020edc0443c733b244c27ead2863b5311946507
Benji Lin <linb@mit.edu>
2015-06-27 14:56:22 -0700

    oops

--- rainbow/importers/google/google_doc_importer.py
+++ rainbow/importers/google/google_doc_importer.py
@@ -4,6 +4,6 @@ import re, urllib.request
 class GoogleDocImporter(Importer):
 
     def __init__(self, googleDocUrl):
-        super()
+        super().__init__()
         googleDocUrlId = re.search('docs.google.com/document/d/([\w-]+)/', googleDocUrl).group(1)
         self.lines = urllib.request.urlopen("https://docs.google.com/document/export?format=txt&id=" + googleDocUrlId).readlines()
