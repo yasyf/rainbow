fbea8494e5216bfa9806363e8f9dfc401d456ea7
Yasyf Mohamedali <yasyfm@gmail.com>
2015-06-28 04:06:38 -0700

    Add Angular map frontend.

--- rainbow/importers/google/google_doc_importer.py
+++ rainbow/importers/google/google_doc_importer.py
@@ -5,14 +5,14 @@ class GoogleDocImporter(Importer):
 
     PATTERN = re.compile(r'd/([\w-]+)/')
     URL = 'https://docs.google.com/document/export?format=txt&id={}'
+    NO_AUTH_TITLE = '<title>Google Docs - create and edit documents online, for free.</title>'
 
     def open(self, url):
         match = self.PATTERN.search(url)
         if match:
             document_id = match.group(1)
-        else:
-            return
-
-        self.data = requests.get(self.URL.format(document_id)).text
+            data = requests.get(self.URL.format(document_id)).text.strip()
+            if self.NO_AUTH_TITLE not in data:
+                self.data = data
 
         return super().open(url)
