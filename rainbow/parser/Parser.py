98fd4e400d15e83af5cb3a02eabcfa90063edbdb
danielzuot <danielzuot@gmail.com>
2015-06-28 02:15:14 -0700

    fixed title selector and skipped invalid events

--- rainbow/parser/Parser.py
+++ rainbow/parser/Parser.py
@@ -7,25 +7,25 @@ class Parser():
     def __init__(self):
         self.oneTimeEventPattern = """
             DATE:{<MONTH><CD><,>*<CD>|<CD><SLASH><CD>(<SLASH><CD>)* }
-            NBAR:
-                {<NN.*|JJ>*<NN.*>}
-
-            NP:
-                {<NBAR >}
+            NP: {<PP\$>?<JJ.*>*<NN.*>+}
         """
 
         self.oneTimeEventChunker = nltk.RegexpParser(self.oneTimeEventPattern)
 
     def parse(self,text):
-        events = [s.strip() for s in text.splitlines()]
+        events = [s.strip() for s in text.strip().splitlines()]
         parsed_events = []
         for event in events:
-            formatted_sentence = process(event, self.oneTimeEventChunker)
-            noun_phrases = next(self.get_terms(formatted_sentence, "NP"))
-            date = next(self.get_terms(formatted_sentence, "DATE"))
-            formatted_date = datetime.datetime.strptime(' '.join(date),"%B %d %Y")
-            formatted_title = ' '.join(noun_phrases)
-            parsed_events.append(OneTimeEvent(date=formatted_date, title=formatted_title).to_dict())
+            if event:
+                formatted_sentence = process(event, self.oneTimeEventChunker)
+                try:
+                    noun_phrase = next(self.get_terms(formatted_sentence, "NP"))
+                    date = next(self.get_terms(formatted_sentence, "DATE"))
+                except StopIteration:
+                    continue
+                formatted_date = datetime.datetime.strptime(' '.join(date),"%B %d %Y")
+                formatted_title = ' '.join(noun_phrase)
+                parsed_events.append(OneTimeEvent(date=formatted_date, title=formatted_title).to_dict())
         return parsed_events
 
 
--- rainbow/parser/process.py
+++ rainbow/parser/process.py
@@ -29,13 +29,12 @@ model = {
 }
 
 def process(sentence, one_time_event_chunker):
-    sentence = sentence.replace('/', ' / ').lower()
+    sentence = sentence.replace('/', ' / ')
     tokens = nltk.word_tokenize(sentence)
     default_tagger = nltk.data.load(nltk.tag._POS_TAGGER)
     tagger = nltk.tag.UnigramTagger(model=model, backoff=default_tagger)
     tagged = tagger.tag(tokens)
     result = one_time_event_chunker.parse(tagged)
-    print(result)
     return result
 
 def date_process(date):
