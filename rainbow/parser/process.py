36a821c4234bf296b5df4bc11e22af39bd0ab17b
Michael Shum <mshum@uber.com>
2015-06-28 03:22:24 -0700

    onetimeevent parsing with recurrent done

--- rainbow/parser/process.py
+++ rainbow/parser/process.py
@@ -1,5 +1,7 @@
 import nltk
 import string
+from recurrent import RecurringEvent
+import datetime
 
 model = {
     '/': 'SLASH',
@@ -29,7 +31,7 @@ model = {
 }
 
 def process(sentence, one_time_event_chunker):
-    sentence = sentence.replace('/', ' / ')
+    sentence = sentence.replace('/', ' / ').lower()
     tokens = nltk.word_tokenize(sentence)
     default_tagger = nltk.data.load(nltk.tag._POS_TAGGER)
     tagger = nltk.tag.UnigramTagger(model=model, backoff=default_tagger)
@@ -38,5 +40,8 @@ def process(sentence, one_time_event_chunker):
     return result
 
 def date_process(date):
-    predicate = lambda x:x not in string.punctuation
-    return filter(predicate, date)
+    table = str.maketrans("", "", string.punctuation)
+    date_string = ' '.join(date).translate(table)
+    r = RecurringEvent(now_date=datetime.datetime(2010, 1, 1))
+    onetimedate = r.parse(date_string)
+    return onetimedate
